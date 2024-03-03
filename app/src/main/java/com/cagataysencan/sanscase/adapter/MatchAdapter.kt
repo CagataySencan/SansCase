3c1053b3f096fcedcd7440e9f91518aaa4120a4b
CagataySencan <cagataysencan07@gmail.com>
2024-03-03 17:35:29 +0300

    recycler view configuration

--- app/src/main/java/com/cagataysencan/sanscase/adapter/MatchAdapter.kt
+++ app/src/main/java/com/cagataysencan/sanscase/adapter/MatchAdapter.kt
@@ -1,7 +1,6 @@
 package com.cagataysencan.sanscase.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -9,22 +8,19 @@ import com.cagataysencan.sanscase.R
 import com.cagataysencan.sanscase.databinding.MatchCardViewBinding
 import com.cagataysencan.sanscase.model.Match
 
-class MatchAdapter(private val matches: ArrayList<Match>) : RecyclerView.Adapter<MatchAdapter.MatchViewHolder>() {
-    inner class MatchViewHolder(private val itemView: MatchCardViewBinding) : RecyclerView.ViewHolder(itemView.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchViewHolder {
+class MatchAdapter(private val matches: List<Match?>) : RecyclerView.Adapter<MatchAdapter.MatchViewHolder>() {
+    inner class MatchViewHolder(var view : MatchCardViewBinding) : RecyclerView.ViewHolder(view.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchAdapter.MatchViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = DataBindingUtil.inflate<MatchCardViewBinding>(inflater, R.layout.match_card_view, parent, false)
         return MatchViewHolder(view)
     }
 
+    override fun onBindViewHolder(holder: MatchAdapter.MatchViewHolder, position: Int) {
+        holder.view.match = matches[position]
+    }
+
     override fun getItemCount(): Int {
         return matches.size
     }
-
-    override fun onBindViewHolder(holder: MatchViewHolder, position: Int) {
-        // TODO
-    }
-
-
 }
